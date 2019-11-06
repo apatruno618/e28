58b84004dc7706fad38cd6f89c2e59406a1a551a
Anna Patruno <apatruno618@gmail.com>
2019-11-06 09:07:14 -0500

    progress on zipfoods after week 9, part 3

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -1,19 +1,28 @@
 <template>
-  <div id="app">
-    <img id="logo" alt="ZipFoods logo" src="./assets/images/zipfoods-logo.png" />
+  <div id='app'>
+    <img id='logo' alt='ZipFoods logo' src='./assets/images/zipfoods-logo.png' />
     <p>ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.</p>
-  <show-products></show-products>
+    <show-featured></show-featured>
+    <show-products></show-products>
   </div>
 </template>
 
 <script>
-import ShowProducts from './components/ShowProducts.vue'
+import ShowProducts from './components/ShowProducts.vue';
+import ShowFeatured from './components/ShowFeatured.vue';
+import { products } from './products.js';
+
 export default {
-  name: "app",
-  components: { ShowProducts }
+  name: 'app',
+  components: { ShowProducts, ShowFeatured },
+  data: function() {
+    return {
+      products: products
+    };
+  }
 };
 </script>
 
 <style lang='scss'>
-@import "./assets/css/zipfoods.scss";
+@import './assets/css/zipfoods.scss';
 </style>
---
+++ zipfoods/src/components/ShowFeatured.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id='featured'>
+    <h2>Featured Products</h2>
+    <ul class='cleanList'>
+      <li v-for='product in featuredProducts' :key='product.id'>{{ product.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { products } from './../products.js';
+
+export default {
+  name: 'ShowFeatured',
+  computed: {
+    featuredProducts: function() {
+      //   goes into each product, goes into category and sees if this is in the category
+      function isMatch(product) {
+        return product.categories.includes(this);
+      }
+      //   filters product data property using isMatch
+      //   returns an array of products that meets our criteria
+      return this.products.filter(isMatch, this.category);
+    }
+  },
+  data: function() {
+    return {
+      category: 'frozen',
+      products: products
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>

8a6b404ffefca5138c11041211f9299dccdb6d84
Anna Patruno <apatruno618@gmail.com>
2019-11-07 21:35:56 -0500

    progress on zipfoods after week 9, part 5

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -4,38 +4,30 @@
 
     <nav>
       <ul>
-        <!-- looping through every link in links -->
         <li v-for='link in links' :key='link'>
-          <!-- hashtag prevents browser from refreshing -->
-          <a href='#' @click='page = link'>{{ link }}</a>
+          <router-link exact :to='paths[link]'>{{ link }}</router-link>
         </li>
       </ul>
     </nav>
-
-    <component :is='linkComponents[page]'></component>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import ShowProducts from './components/ShowProducts.vue';
-import ShowHome from './components/ShowHome.vue';
-import ShowCategories from './components/ShowCategories.vue';
 import { products } from './products.js';
 
 export default {
   name: 'app',
-  components: { ShowProducts, ShowHome, ShowCategories },
+  components: {},
   data: function() {
     return {
       products: products,
-      /* Default page to start with */
-      page: 'home',
-      /* Store links in an array to maintain order */
+      // src/App.vue data properties
       links: ['home', 'products', 'categories'],
-      linkComponents: {
-        home: 'ShowHome',
-        products: 'ShowProducts',
-        categories: 'ShowCategories'
+      paths: {
+        home: '/',
+        products: '/products',
+        categories: '/categories'
       }
     };
   }
--- zipfoods/src/main.js
+++ zipfoods/src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+import ShowProducts from './components/ShowProducts.vue';
+import ShowHome from './components/ShowHome.vue';
+import ShowCategories from './components/ShowCategories.vue';
 
+Vue.use(VueRouter);
 Vue.config.productionTip = false
 
+const routes = [
+  // each route is an entry point into the application
+  { path: '/', component: ShowHome },
+  { path: '/products', component: ShowProducts },
+  { path: '/categories', component: ShowCategories }
+]
+
+const router = new VueRouter({
+  routes: routes,
+  // takes advantage of browser API to dynamically change url in browser without page request
+  mode: 'history',
+});
+
 new Vue({
+  router: router,
   render: h => h(App),
 }).$mount('#app')

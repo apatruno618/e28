af165ffcc64198ed8763c5f2bc7aa57631b92ef9
Anna Patruno <apatruno618@gmail.com>
2019-11-19 13:38:52 -0500

    content additions

--- p3/src/App.vue
+++ p3/src/App.vue
@@ -1,22 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='app'>
+    <img id='logo' alt='TurkeyTable logo' src='./assets/turkeytable-logo.png' />
+    <HelloWorld msg='Welcome to Your Vue.js App' />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'app',
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
+@import './assets/css/turkeytable.css';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
